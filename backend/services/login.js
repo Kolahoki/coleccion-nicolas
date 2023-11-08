0b05256d7f5be2ca9aa5e92057aa7aebad231667
Kolahoki <nicolax449@gmail.com>
2023-11-08 09:26:24 +0000

    nuevo branch con ut2a3 desde clase

--- backend/services/login.js
+++ backend/services/login.js
@@ -1,19 +1,19 @@
-const db = require('./db')
-const helper = require('../helper')
-const config = require('../config')
+const db = require("./db");
+const helper = require("../helper");
+const config = require("../config");
 
 async function getUserData(user, password) {
-    const rows = await db.query(`
+  const rows = await db.query(`
             SELECT * FROM usuarios WHERE login='${user}' AND password='${password}';
-    `)
+    `);
 
-    const data = helper.emptyOrRows(rows[0])
+  const data = helper.emptyOrRows(rows[0]);
 
-    return {
-        data
-    }
+  return {
+    data,
+  };
 }
 
 module.exports = {
-    getUserData
-}
+  getUserData,
+};
